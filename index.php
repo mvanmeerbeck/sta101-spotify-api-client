4fd83c48513dac1169dde3bb25dbeb4b34e00465
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2018-02-16 23:23:30 +0100

    500 tracks

--- index.php
+++ index.php
@@ -3,60 +3,15 @@
 require __DIR__ . '/vendor/autoload.php';
 
 /**
- * classical,rock,rap,jazz,reggae 10
+ * classical,rock,rap,jazz,reggae 50 2
  */
 
 $token = $argv[1];
 $genres = explode(',', $argv[2]);
 $limit = $argv[3];
+$pages = $argv[4];
 
-$tracks = [];
 $client = new GuzzleHttp\Client();
-
-foreach ($genres as $genre) {
-    $response = $client->request('GET', "https://api.spotify.com/v1/search?q=genre%3A$genre&type=track&limit=$limit&market=FR", [
-        'verify' => false,
-        'headers' => [
-            'Authorization' => "Bearer $token",
-        ]
-    ]);
-
-    $response = json_decode($response->getBody(), true);
-
-    foreach ($response['tracks']['items'] as $item) {
-        $audioFeatures = json_decode($client->request('GET', 'https://api.spotify.com/v1/audio-features/' . $item['id'], [
-            'verify' => false,
-            'headers' => [
-                'Authorization' => "Bearer $token",
-            ]
-        ])->getBody(), true);
-
-        $tracks[] = [
-            'id' => $item['id'],
-            'name' => $item['name'],
-            'artist' => $item['artists'][0]['name'],
-            'popularity' => $item['popularity'],
-            'genre' => $genre,
-            'danceability' => $audioFeatures['danceability'],
-            'energy' => $audioFeatures['energy'],
-            'key' => $audioFeatures['key'],
-            'loudness' => $audioFeatures['loudness'],
-            'mode' => $audioFeatures['mode'],
-            'speechiness' => $audioFeatures['speechiness'],
-            'acousticness' => $audioFeatures['acousticness'],
-            'instrumentalness' => $audioFeatures['instrumentalness'],
-            'liveness' => $audioFeatures['liveness'],
-            'valence' => $audioFeatures['valence'],
-            'tempo' => $audioFeatures['tempo'],
-            'duration_ms' => $audioFeatures['duration_ms'],
-            'time_signature' => $audioFeatures['time_signature'],
-        ];
-
-        sleep(1);
-    }
-
-    sleep(1);
-}
 
 $handle = fopen('tracks.csv', 'w');
 
@@ -81,14 +36,63 @@ fputcsv($handle, [
     'time_signature',
 ]);
 
-foreach ($tracks as $track) {
-    foreach ($track as $i => $value) {
-        if (is_float($value)) {
-            $track[$i] = sprintf('%f', $value);
-        }
-    }
+foreach ($genres as $genre) {
+    echo $genre . PHP_EOL;
+    for ($i = 0; $i < $pages; $i++) {
+        echo 'page ' . $i . PHP_EOL;
+        $offset = $i * $limit;
 
-    fputcsv($handle, $track);
+        $response = $client->request('GET', "https://api.spotify.com/v1/search?q=genre%3A$genre&type=track&offset=$offset&limit=$limit&market=FR", [
+            'verify' => false,
+            'headers' => [
+                'Authorization' => "Bearer $token",
+            ]
+        ]);
+
+        $response = json_decode($response->getBody(), true);
+
+        foreach ($response['tracks']['items'] as $item) {
+            $audioFeatures = json_decode($client->request('GET', 'https://api.spotify.com/v1/audio-features/' . $item['id'], [
+                'verify' => false,
+                'headers' => [
+                    'Authorization' => "Bearer $token",
+                ]
+            ])->getBody(), true);
+
+            $track = [
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'artist' => $item['artists'][0]['name'],
+                'popularity' => $item['popularity'],
+                'genre' => $genre,
+                'danceability' => $audioFeatures['danceability'],
+                'energy' => $audioFeatures['energy'],
+                'key' => $audioFeatures['key'],
+                'loudness' => $audioFeatures['loudness'],
+                'mode' => $audioFeatures['mode'],
+                'speechiness' => $audioFeatures['speechiness'],
+                'acousticness' => $audioFeatures['acousticness'],
+                'instrumentalness' => $audioFeatures['instrumentalness'],
+                'liveness' => $audioFeatures['liveness'],
+                'valence' => $audioFeatures['valence'],
+                'tempo' => $audioFeatures['tempo'],
+                'duration_ms' => $audioFeatures['duration_ms'],
+                'time_signature' => $audioFeatures['time_signature'],
+            ];
+
+            foreach ($track as $j => $value) {
+                if (is_float($value)) {
+                    $track[$j] = sprintf('%f', $value);
+                }
+            }
+
+            fputcsv($handle, $track);
+
+            sleep(1);
+        }
+
+        sleep(1);
+    }
 }
 
 fclose($handle);
